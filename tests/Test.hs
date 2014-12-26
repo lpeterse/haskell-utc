62f9efb1df0e5e6a47a4b5523c917a5dc76ee4cf
Lars Petersen <lars@nyantec.com>
2014-12-26 22:23:46 +0100

    Using QuickCheck for tests.

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,32 +1,28 @@
 module Test ( tests ) where
 
 import Distribution.TestSuite
+import Distribution.TestSuite.QuickCheck
+
+import Test.QuickCheck
 
 import Data.Tempus
 
+import Data.Word
+
 tests :: IO [Test]
 tests = return 
-         [ Test test001
-         , Test test002
+         [ test001
+         , test002
          ]
 
-test001 :: TestInstance
-test001 = TestInstance
-        { name = "show epoch"
-        , tags = []
-        , options = []
-        , setOption = \_ _ -> Right test001
-        , run = do return $ Finished 
-                          $ if show epoch == "1970-01-01T00:00:00Z"
-                              then Pass
-                              else Fail (show epoch)
-        }
+test001 :: Test
+test001
+  = testProperty "test001"
+  $ show epoch === "1970-01-01T00:00:00Z"
 
-test002 :: TestInstance
-test002 = TestInstance
-        { run = return $ Finished $ Fail "Always fails!"
-        , name = "fails"
-        , tags = []
-        , options = []
-        , setOption = \_ _ -> Right test001
-        }
+test002 :: Test
+test002
+  = testProperty "test002"
+  $ forAll 
+      (choose (0, 1000000000))
+      (\x-> x == toUnixTime (fromUnixTime x))
