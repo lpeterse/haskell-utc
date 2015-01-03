2c14dfbc949e9a654e5d7e009d2612de4f1502d1
Lars Petersen <lars@nyantec.com>
2015-01-03 18:03:39 +0100

    Fixed previously added test case.

--- Data/Tempus/GregorianTime.hs
+++ Data/Tempus/GregorianTime.hs
@@ -27,6 +27,7 @@ module Data.Tempus.GregorianTime
   , isLeapYear'
   , yearToDays
   , selectMonthMarFeb
+  , shrinkYearMarFeb
   ) where
 
 import Control.Monad
@@ -361,7 +362,11 @@ toUnixTime t
 --   https://stackoverflow.com/questions/1274964/how-to-decompose-unix-time-in-c
 fromUnixTime :: MonadPlus m => UnixTime -> m GregorianTime
 fromUnixTime (UnixTime i)
-  = do yearMarFeb                   <- shrinkYearMarFeb 0 9999
+  = do -- adjust the epoch to the year 0000
+       days                          = i `div` (24*60*60*1000) + 719499
+       -- calculate the "year" whereas a year ranges from March 1 to February 28|29
+       -- having the leap days at the end of the year allows for some tricks
+       yearMarFeb                   <- shrinkYearMarFeb days 0 9999
        let remainingDays             = days - (yearToDays yearMarFeb)
        let monthMarFeb               = selectMonthMarFeb remainingDays
        let (yearJanDec, monthJanDec) = if monthMarFeb > 10
@@ -370,45 +375,43 @@ fromUnixTime (UnixTime i)
        validate $ GregorianTime
                { gdtYear         = fromIntegral yearJanDec
                , gdtMonth        = fromIntegral monthJanDec
-               , gdtDay          = fromIntegral $ remainingDays - (367 * monthMarFeb `quot` 12)
-               , gdtMinutes      = fromIntegral $ abs i `quot` 60000 `rem` (24*60)
-               , gdtMilliSeconds = fromIntegral $ abs i `rem` 60000
+               , gdtDay          = fromIntegral $ remainingDays - (367 * monthMarFeb `div` 12)
+               , gdtMinutes      = fromIntegral $ i `div` 60000 `mod` (24*60)
+               , gdtMilliSeconds = fromIntegral $ i `mod` 60000
                , gdtOffset       = OffsetMinutes 0
                }
+
+shrinkYearMarFeb :: MonadPlus m => Int64 -> Int64 -> Int64 -> m Int64
+shrinkYearMarFeb days lower upper
+  -- we found the year satifying the condition
+  | lower == upper                      = return lower
+  -- just a fail-safe recursion breaker
+  | lower > upper                       = mzero
+  -- the tested year has more or equally many days than what we are looking for
+  -- induction guarantee: unless 'lower == upper' (catched above) it always holds 'mid < upper'
+  | days <= yearToDays (mid   + 1) + 30 = shrinkYearMarFeb days lower mid
+  -- the tested year has less days than what we are looking for
+  -- induction guarantee: it always holds 'mid + 1 > lower'
+  | days >  yearToDays (mid   + 1) + 30 = shrinkYearMarFeb days (mid + 1) upper
+  -- should not happen
+  | otherwise                           = mzero
   where
-    -- days from 0000-01-01
-    days 
-      = i `quot` (24*60*60*1000) + 719499
-    shrinkYearMarFeb lower upper
-      -- we found the year satifying the condition
-      | lower == upper                      = return lower
-      -- just a fail-safe recursion breaker
-      | lower > upper                       = mzero
-      -- the tested year has more or equally many days than what we are looking for
-      -- induction guarantee: unless 'lower == upper' (catched above) it always holds 'mid < upper'
-      | days <= yearToDays (mid   + 1) + 30 = shrinkYearMarFeb lower mid
-      -- the tested year has less days than what we are looking for
-      -- induction guarantee: it always holds 'mid + 1 > lower'
-      | days >  yearToDays (mid   + 1) + 30 = shrinkYearMarFeb (mid + 1) upper
-      -- should not happen
-      | otherwise                           = mzero
-      where
-        mid = (lower + upper) `quot` 2
+    mid = (lower + upper) `div` 2
 
 selectMonthMarFeb :: Int64 -> Int64
 selectMonthMarFeb d
-      | d <= 367 *  2 `quot` 12 = 1
-      | d <= 367 *  3 `quot` 12 = 2
-      | d <= 367 *  4 `quot` 12 = 3
-      | d <= 367 *  5 `quot` 12 = 4
-      | d <= 367 *  6 `quot` 12 = 5
-      | d <= 367 *  7 `quot` 12 = 6
-      | d <= 367 *  8 `quot` 12 = 7
-      | d <= 367 *  9 `quot` 12 = 8
-      | d <= 367 * 10 `quot` 12 = 9
-      | d <= 367 * 11 `quot` 12 = 10
-      | d <= 367                = 11
-      | otherwise               = 12
+      | d <= 367 *  2 `div` 12 = 1
+      | d <= 367 *  3 `div` 12 = 2
+      | d <= 367 *  4 `div` 12 = 3
+      | d <= 367 *  5 `div` 12 = 4
+      | d <= 367 *  6 `div` 12 = 5
+      | d <= 367 *  7 `div` 12 = 6
+      | d <= 367 *  8 `div` 12 = 7
+      | d <= 367 *  9 `div` 12 = 8
+      | d <= 367 * 10 `div` 12 = 9
+      | d <= 367 * 11 `div` 12 = 10
+      | d <= 367               = 11
+      | otherwise              = 12
 
 
 isLeapYear' :: Int -> Bool
