d8167d8dc35894dabd5cc7a6cff84a50b6187c3e
Lars Petersen <lars@nyantec.com>
2015-01-12 23:15:26 +0100

    Added add-functions to IsTime class.

--- Data/Tempus/Class/IsTime.hs
+++ Data/Tempus/Class/IsTime.hs
@@ -3,6 +3,10 @@ module Data.Tempus.Class.IsTime
   ( IsTime (..)
   ) where
 
+import Data.Ratio
+
+import Data.Tempus.Internal
+
 class IsTime t where
   -- | > hour           "2014-⁠12-⁠24T18:11:47.042Z" ==          18
   hour                  :: t -> Integer
@@ -16,5 +20,40 @@ class IsTime t where
   setMinute             :: (Monad m) => Integer  -> t -> m t
   setSecond             :: (Monad m) => Integer  -> t -> m t
   setSecondFraction     :: (Monad m) => Rational -> t -> m t
+
+  addHours              :: (Monad m) => Integer  -> t -> m t
+  addHours h t
+    = if days /= 0
+        then fail "IsTime.addHours: out of range"
+        else setHour hors t
+    where
+      h'   = h + (hour t)
+      hors = h' `mod` hoursPerDay
+      days = h' `div` hoursPerDay
+
+  addMinutes            :: (Monad m) => Integer  -> t -> m t
+  addMinutes m t
+    = setMinute mins t >>= addHours hors
+    where
+      m'   = m + (minute t)
+      mins = m' `mod` minsPerHour
+      hors = m' `div` minsPerHour
+
+  addSeconds            :: (Monad m) => Integer  -> t -> m t
+  addSeconds s t
+    = setSecond secs t >>= addMinutes mins
+    where
+      s'   = s + (second t)
+      secs = s' `mod` secsPerMinute
+      mins = s' `div` secsPerMinute
+
+  addSecondFractions    :: (Monad m) => Rational -> t -> m t
+  addSecondFractions f t
+    = setSecondFraction frcs t >>= addSeconds secs
+    where
+      f'   = f + (secondFraction t)
+      frcs = f' - (truncate f' % 1)
+      secs = truncate f'
+
   -- | The beginning of a day: 00:00:00
   midnight :: t
