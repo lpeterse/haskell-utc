ec60a3b9c3a47d4363c070cf70e48a90194abbc9
Lars Petersen <lars@nyantec.com>
2015-01-13 00:23:07 +0100

    Module exposing Class modules.

---
+++ Data/Tempus/Class.hs
@@ -0,0 +1,15 @@
+module Data.Tempus.Class
+  ( module Data.Tempus.Class.Epoch
+  , module Data.Tempus.Class.Midnight
+  , module Data.Tempus.Class.IsDate
+  , module Data.Tempus.Class.IsTime
+  , module Data.Tempus.Class.IsUnixTime
+  , module Data.Tempus.Class.HasUnixTime
+  ) where
+
+import Data.Tempus.Class.Epoch
+import Data.Tempus.Class.Midnight
+import Data.Tempus.Class.IsDate
+import Data.Tempus.Class.IsTime
+import Data.Tempus.Class.IsUnixTime
+import Data.Tempus.Class.HasUnixTime
